19688b8f19f1e0cc1773e1344426aafd24fbe620
nadiamaria <nadiamaria353@gmail.com>
2020-10-03 20:30:32 +0300

    configFirebase

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,13 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAvraHaYYQQV9xis3h0rJgrIkcBzMGK8BQ",
-    authDomain: "test-curs07-react-2.firebaseapp.com",
-    databaseURL: "https://test-curs07-react-2.firebaseio.com",
-    projectId: "test-curs07-react-2",
-    storageBucket: "test-curs07-react-2.appspot.com",
-    messagingSenderId: "671843276537",
-    appId: "1:671843276537:web:c2ea78901889ef0c507428",
-    measurementId: "G-7FE5VP4MW2"
-  };
+  apiKey: "AIzaSyAvraHaYYQQV9xis3h0rJgrIkcBzMGK8BQ",
+  authDomain: "test-curs07-react-2.firebaseapp.com",
+  databaseURL: "https://test-curs07-react-2.firebaseio.com",
+  projectId: "test-curs07-react-2",
+  storageBucket: "test-curs07-react-2.appspot.com",
+  messagingSenderId: "671843276537",
+  appId: "1:671843276537:web:c2ea78901889ef0c507428",
+  measurementId: "G-7FE5VP4MW2"
+};
 
 export default firebaseConfig;
